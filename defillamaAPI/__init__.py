ab5f7029316b67b885a7dd747fd4c29f79c3af27
jafark92 <jsadiqk92@gmail.com>
2023-06-13 03:53:22 -0500

    updated to use classes directly as object

--- defillamaAPI/__init__.py
+++ defillamaAPI/__init__.py
@@ -7,3 +7,13 @@ from .abidecoder import ABIDecoder
 from .bridges import Bridges
 from .volumes import Volumes
 from .feesandrevenue import FeesAndRevenue
+
+# So that we can directly used as object during import instead of creating obvious objects
+FeesAndRevenue = FeesAndRevenue()
+TVL = TVL()
+Coins = Coins()
+Stablecoins = Stablecoins()
+Yields = Yields()
+ABIDecoder = ABIDecoder()
+Bridges = Bridges()
+Volumes = Volumes()
